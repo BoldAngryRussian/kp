ec54f02b112af05861078166de72d59a23d8352c
BoldAngryRussian <caterpillar.alexey@gmail.com>
2025-06-12 00:39:04 +0500

    Первая страница создания КП

--- ui/src/layouts/kp-creation/kp-creation-start.js
+++ ui/src/layouts/kp-creation/kp-creation-start.js
@@ -1,30 +1,85 @@
 import MDBox from "components/MDBox";
 import MDButton from "components/MDButton";
+import uylaLogo from "assets/images/uyta-logo.png"
 
 export default function KPCreationStart() {
     return (
         <MDBox
             display="flex"
             justifyContent="center"
+            flexDirection="column"
             alignItems="center"
-            height="80vh" // высота всей видимой области экрана
+            height="60vh" // высота всей видимой области экрана
+            gap={3}
+            sx={{
+                //border: "1px solid #ccc"
+            }}
         >
-            <MDButton
-                variant="text"
-                color="secondary"
-                size="small"
+            <MDBox
                 sx={{
-                    fontSize: "1rem",
-                    textTransform: "none",
-                    border: "1px solid #999",
-                    backgroundColor: "#f0f0f0",
-                    "&:hover": {
-                        backgroundColor: "#e0e0e0",
-                    },
+                    //border: "1px solid #ccc",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",         // Убрать ограничение по ширине
+                    maxWidth: "1000px",    // Добавить максимум, если нужно
+                    margin: "0 auto",      // Центровка
                 }}
             >
-                загрузить с вашего устройства
-            </MDButton>
+                <img
+                    src={uylaLogo}
+                    alt="UYLA Logo"
+                    style={{
+                        width: "100%",
+                        maxWidth: "700px",
+                        height: "auto",
+                        filter: "grayscale(100%)", // делаем чёрно-белым
+                        opacity: 0.05,
+                    }}
+                />
+            </MDBox>
+
+            <MDBox>
+                <MDBox>
+                    <MDButton
+                        variant="text"
+                        color="secondary"
+                        size="small"
+                        sx={{
+                            width: '100%',
+                            minWidth: '300px',
+                            fontSize: "1rem",
+                            textTransform: "none",
+                            border: "1px solid #999",
+                            backgroundColor: "#f0f0f0",
+                            "&:hover": {
+                                backgroundColor: "#e0e0e0",
+                            },
+                        }}
+                    >
+                        Создать
+                    </MDButton>
+                </MDBox>
+                <MDBox pt={2}>
+                    <MDButton
+                        variant="text"
+                        color="secondary"
+                        size="small"
+                        sx={{
+                            width: '100%',                            
+                            fontSize: "1rem",
+                            textTransform: "none",
+                            border: "1px solid #999",
+                            backgroundColor: "#f0f0f0",
+                            "&:hover": {
+                                backgroundColor: "#e0e0e0",
+                            },
+                        }}
+                    >
+                        Редактировать
+                    </MDButton>
+                </MDBox>
+            </MDBox>
         </MDBox>
     )
 }
